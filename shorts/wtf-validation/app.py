946bb7599a60c55b14bd332f0088eb4afc93b88e
ks <mugoh.ks@gmail.com>
2018-11-23 16:31:19 +0300

    Redirect POST request

--- shorts/wtf-validation/app.py
+++ shorts/wtf-validation/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect, session
 from form_validate import Contact
 
 app = Flask(__name__)
@@ -8,12 +8,12 @@ app.config.update(
 
 @app.route('/form', methods=['GET', 'POST'])
 def contact():
-    name = ''
+
     form = Contact()
     if form.validate_on_submit():
-        name = form.name.data
-        # return render_template('form_success.html', form=form, name=name)
-    return render_template('user.html', form=form, name=name)
+        session['name'] = form.name.data
+        return redirect(url_for('contact'))
+    return render_template('user.html', form=form, name=session.get('name'))
 
 
 if __name__ == '__main__':
